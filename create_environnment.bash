41c669d74cd712ff158a1f6a74b8b2c10410c880
Hansastro <jean-phi@laposte.net>
2017-11-26 21:00:05 +0100

    Add the a script to help to create the virtual environment

---
+++ create_environnment.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Creating python virtual environment...
+echo -------------------------------------
+virtualenv env
+source env/bin/activate
+
+echo
+echo Installing the required python packages...
+echo ------------------------------------------
+pip install -r requirements.txt
+
+echo
+echo ------------------------------------------------------------------
+echo type \'source /env/bin/activate\' to activate the virtual environment
+echo type \'deactivate\' to quit the virtual environment
+echo ------------------------------------------------------------------
